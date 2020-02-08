53b17364c776758be5d981a1d37b3a2b8d7c9693
MikeJollie2707 <bachngo@yahoo.com>
2020-02-08 01:35:25 -0800

    Created new test
    sortCard usually throw seg fault

--- src/test.cpp
+++ src/test.cpp
@@ -4,11 +4,31 @@
 int main()
 {
     Deck deck;
-    while (deck.numCard() >= 0)
+    deck.shuffle();
+
+    Player player("MikeJollie");
+    int times = 4;
+    for (int i = 0; i < times; i++)
     {
-        Card** card = deck.drawCard();
-        std::cout << (*card)->getNumber() << " " << (*card)->getSuit() << " " << (*card)->getValue();
+        Card** draw = deck.drawCard();
+        std::cout << (*draw)->getNumber() << " " << (*draw)->getSuit() << " " << (*draw)->getValue();
+        std::cout << std::endl;
+        player.receiveCard(draw);
+    }
+    std::cout << "\n\n\n";
+    for (int i = 0; i < times; i++)
+    {
+        player.setIndex(i);
+        player.selectCard();
+    }
+    
+    std::vector<Card**> cards = player.playCard();
+
+    for (int i = 0; i < times; i++)
+    {
+        std::cout << (*(cards[i]))->getNumber() << " " << (*(cards[i]))->getSuit() << " " << (*(cards[i]))->getValue();
         std::cout << std::endl;
     }
+
     return 0;
 }
