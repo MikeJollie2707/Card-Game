edcce6e4ed89c45aa7948e1356d5ba9353966fe6
MikeJollie2707 <bachngo@yahoo.com>
2020-02-07 23:55:17 -0800

    Created test case

--- src/test.cpp
+++ src/test.cpp
@@ -1,6 +1,14 @@
 #include "player.h"
+#include <iostream>
 
 int main()
 {
+    Deck deck;
+    while (deck.numCard() >= 0)
+    {
+        Card** card = deck.drawCard();
+        std::cout << (*card)->getNumber() << " " << (*card)->getSuit() << " " << (*card)->getValue();
+        std::cout << std::endl;
+    }
     return 0;
 }
