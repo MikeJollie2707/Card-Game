26b17651bb6cd8aef11d62c0d21b499e64fb384b
MikeJollie2707 <bachngo@yahoo.com>
2020-02-08 23:18:20 -0800

    Modified test

--- src/test.cpp
+++ src/test.cpp
@@ -5,10 +5,27 @@ int main()
 {
     Deck deck;
     Player player;
-    player.drawFromDeck(deck);
-    player.setIndex(0);
-    player.tselectCard();
-    List<Node<Card>*> cards = player.tplayCard();
-    std::cout << cards[0] << std::endl;
+    int times = 2;
+    std::cout << "Card drown: " << std::endl;
+    for (int i = 0; i < times; i++)
+    {
+        Node<Card>* card = deck.drawtCard();
+        std::cout << card->data.getNumber() << " " << card->data.getSuit() << " " << card->data.getValue();
+        std::cout << std::endl;
+        player.treceiveCard(card);
+        player.setIndex(i);
+        std::cout << "Ok boomer\n";
+        player.tselectCard();
+        std::cout << "Seg fault?\n";
+        
+    }
+    List<Card*> cards = player.tplayCard();
+    std::cout << "Hand: " << std::endl;
+    for (int i = 0; i < times; i++)
+    {
+        std::cout << cards[i].data->getNumber() << " " << cards[i].data->getSuit() << " " << cards[i].data->getValue();
+        std::cout << std:: endl;
+    }
+
     return 0;
 }
